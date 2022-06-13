61c2579c581030b3404a3baad99559fc11bf762f
raisgil <raisgil@vk.com>
2022-06-13 19:17:20 +0300

    80% of homework is done

--- src/main/java/impl/ThinkOfNumberGame.java
+++ src/main/java/impl/ThinkOfNumberGame.java
@@ -22,19 +22,18 @@ public class ThinkOfNumberGame implements Game {
         System.out.print("Сделайте ставку: ");
         long bet = new Scanner(System.in).nextLong();
         if (user.getMoney() < bet) {
-            System.out.println("У вас нет столько денег! Баланс: "+user.getMoney());
+            System.out.println("У вас нет столько денег! Баланс: " + user.getMoney());
             return makeBet();
         } else return bet;
-    }
+    } //Проверка ставки
 
     @Override
     public void play() {
         long bet = makeBet();
-        // TODO После рефакторинга метода checkUserReady убрать неиспользуюмую переменную isUserRady.
-        String isUserReady = checkUserReady();
+        checkUserReady();
         int currentTry = 0;
         int numberToGuess = new Random().nextInt(101);
-        if (guessUsersNumber(numberToGuess, currentTry)) {
+        if (guessUsersNumber(numberToGuess, currentTry, currentMinNumber, currentMaxNumber)) {
             user.reduceMoney(bet);
         } else {
             user.increaseMoney(bet);
@@ -43,43 +42,48 @@ public class ThinkOfNumberGame implements Game {
 
     // TODO Вынести методы в абстрактный класс
 
-    private String checkUserReady() {
-        //TODO Проверить что ввел пользователь.
-        //   Если пользователь ввел что-то кроме 'Загадал' или ничего, то указать ему что надо ввести 'Загадал' и попрость заново.
-        //   Метод должен ничего не возвращать, т.е. String станет void.
+    private void checkUserReady() {
         System.out.println("Загадайте число от 0 до 100 и вбейте 'Загадал'.");
-        return new Scanner(System.in).nextLine();
-    }
+        String ready = new Scanner(System.in).nextLine();
+        if (!ready.equalsIgnoreCase("Загадал")) {
+            checkUserReady();
+        }
+    } // Проверка на готовность
 
-    private boolean guessUsersNumber(int numberToGuess, int currentTry) {
-        //TODO оличество попыток не учитывается
-        //   Программа угадывает число больше, чем максимальное количество попыток.
+    private boolean guessUsersNumber(int numberToGuess, int currentTry, int currentMinNumber, int currentMaxNumber) {
         if (checkCountOfTries(currentTry)) {
-            System.out.println("Программа исчерпала кол-во попыток");
+            System.out.println("Программа исчерпала кол-во попыток, Вы выиграли!");
+            return false;
+        } else {
+            System.out.println("Ваше число - " + numberToGuess + "? \n " +
+                    "Введите:\n" +
+                    "Да\n" +
+                    "Больше\n" +
+                    "Меньше");
+            String userAnswer = new Scanner(System.in).nextLine();
+            if (userAnswer.equalsIgnoreCase("да")) {
+                System.out.println("Проигрыш! Программа угадала число");
+                return true;
+            } else if (userAnswer.equalsIgnoreCase("больше")) {
+                //применено бинарное сечение
+                int newNumberToGuess = (currentMaxNumber + numberToGuess) / 2;
+                if (numberToGuess == newNumberToGuess) {
+                    newNumberToGuess++;
+                }
+                guessUsersNumber(newNumberToGuess, ++currentTry, ++numberToGuess, currentMaxNumber);
+            } else if (userAnswer.equalsIgnoreCase("меньше")) {
+                int newNumberToGuess = (currentMinNumber + numberToGuess) / 2;
+                if (numberToGuess == newNumberToGuess) {
+                    newNumberToGuess--;
+                }
+                guessUsersNumber(newNumberToGuess, ++currentTry, currentMinNumber, --numberToGuess);
+            }
             return false;
         }
-        System.out.println("Ваше число - " + numberToGuess + "? \n " +
-                "Введите:\n" +
-                "Да\n" +
-                "Больше\n" +
-                "Меньше");
-        String userAnswer = new Scanner(System.in).nextLine();
-        if (userAnswer.equalsIgnoreCase("да")) {
-            System.out.println("Проигрыш! Программа угадала число");
-        } else if (userAnswer.equalsIgnoreCase("больше")) {
-            //TODO применить бинарное сечение
-            int newNumberToGuess = (currentMaxNumber - numberToGuess) / 2 + numberToGuess;
-            currentMinNumber = numberToGuess;
-            guessUsersNumber(newNumberToGuess, currentTry++);
-        } else if (userAnswer.equalsIgnoreCase("меньше")) {
-            int newNumberToGuess = numberToGuess - (numberToGuess - currentMinNumber);
-            currentMaxNumber = numberToGuess;
-            guessUsersNumber(newNumberToGuess, currentTry++);
-        }
-        return false;
     }
+
 
     private boolean checkCountOfTries(int currentTry) {
         return currentTry == COUNT_OF_TRIES;
-    }
+    } //учитывается количество попыток
 }
